d07fb80752d72092862f2f1b3cd43082ef3fa268
Erik Pihl <pierik@14.ed4225.lab.cse.rh69.ii.2.hb.s.cdal.chalmers.se>
2017-09-04 10:32:33 +0200

    Lab 1 solution

---
+++ lab1_ip6.cpp
@@ -0,0 +1,114 @@
+/********************************************************* -- SOURCE -{{{1- */
+/** Translate host name into IPv4
+ *
+ * Resolve IPv4 address for a given host name. The host name is specified as
+ * the first command line argument to the program. 
+ *
+ * Build program:
+ *  $ gcc -Wall -g -o resolve <file>.cpp
+ */
+/******************************************************************* -}}}1- */
+
+#include <stdio.h>
+#include <stddef.h>
+
+#include <assert.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <strings.h>
+
+
+
+//--//////////////////////////////////////////////////////////////////////////
+//--    local declarations          ///{{{1///////////////////////////////////
+
+void print_usage( const char* aProgramName );
+
+//--    local config                ///{{{1///////////////////////////////////
+
+/* HOST_NAME_MAX may be missing, e.g. if you're running this on an MacOS X
+ * machine. In that case, use MAXHOSTNAMELEN from <sys/param.h>. Otherwise 
+ * generate an compiler error.
+ */
+#if !defined(HOST_NAME_MAX)
+#	if defined(__APPLE__)
+#		include <sys/param.h>
+#		define HOST_NAME_MAX MAXHOSTNAMELEN
+#	else  // !__APPLE__
+#		error "HOST_NAME_MAX undefined!"
+#	endif // ~ __APPLE__
+#endif // ~ HOST_NAME_MAX
+
+//--    main()                      ///{{{1///////////////////////////////////
+int main( int aArgc, char* aArgv[] )
+{
+	// Check if the user supplied a command line argument.
+	if( aArgc != 2 )
+	{
+		print_usage( aArgv[0] );
+		return 1;
+	}
+
+	// The (only) argument is the remote host that we should resolve.
+	const char* remoteHostName = aArgv[1];
+
+	// Get the local host's name (i.e. the machine that the program is 
+	// currently running on).
+	const size_t kHostNameMaxLength = HOST_NAME_MAX+1;
+	char localHostName[kHostNameMaxLength];
+   
+	if( -1 == gethostname( localHostName, kHostNameMaxLength ) )
+	{
+		perror( "gethostname(): " );
+		return 1;
+	}
+
+	// Print the initial message
+	printf( "Resolving `%s' from `%s':\n", remoteHostName, localHostName );
+
+	struct addrinfo hints;
+	bzero(&hints, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	struct addrinfo *result;
+	bzero(&result, sizeof(result));
+	int s = getaddrinfo(remoteHostName, NULL, &hints, &result) < 0;
+
+	if(s != 0){
+		printf("Error in getaddrinfo(): %s\n",gai_strerror(s));
+		return 1;
+	}
+	for(addrinfo const *ptr = result; ptr; ptr = ptr->ai_next){
+		sockaddr *sockAddr = ptr->ai_addr;
+		char buf[50];		
+		if(sockAddr->sa_family == AF_INET){
+			sockaddr_in* inAddr = (sockaddr_in*) sockAddr; 
+			inet_ntop(AF_INET, &inAddr->sin_addr, buf , 50); 
+			printf("IPv4 address: %s\n", buf);
+		}
+		if(sockAddr->sa_family == AF_INET6){
+			sockaddr_in6* inAddr = (sockaddr_in6*)sockAddr;
+			inet_ntop(AF_INET6, &inAddr->sin6_addr, buf, 50);
+			printf("IPv6 address: %s\n", buf);
+		}
+
+	}
+	
+
+	// Ok, we're done. Return success.
+	return 0;
+}
+
+
+//--    print_usage()               ///{{{1///////////////////////////////////
+void print_usage( const char* aProgramName )
+{
+	fprintf( stderr, "Usage: %s <hostname>\n", aProgramName );
+}
+
+//--///}}}1/////////////// vim:syntax=cpp:foldmethod=marker:ts=4:noexpandtab: 
